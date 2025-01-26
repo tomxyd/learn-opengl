402e419be77f8ef5cb1f587a5fd62027b23cf16c
Tolulope David <toludavid39@gmail.com>
2025-01-25 21:24:46 -0700

    Made all boxes rotate

--- Source.cpp
+++ Source.cpp
@@ -227,7 +227,7 @@ int main()
         {
             glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
+			float angle = 20.0f * (i + 1);
 			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 36);
